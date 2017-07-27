898046dee3c87cbec813268d3893ec776d9acc4a
WIN\AIT-UNF-GDC <frogkr@gmail.com>
2017-07-27 11:13:38 +0200

    Added a simi working Mainmenu

--- 4P Hitman/Assets/scripts/MMplanerotate.cs
+++ 4P Hitman/Assets/scripts/MMplanerotate.cs
@@ -2,24 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MMplanerotate : MonoBehaviour {
 
     public float spinbot = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+
+
+        GameObject.Find("ExitButton").GetComponent<Button>().onClick.AddListener(OnExitClick);
+        GameObject.Find("CreditsButton").GetComponent<Button>().onClick.AddListener(OnCreditsClick);
+        GameObject.Find("FFAButton").GetComponent<Button>().onClick.AddListener(OnFFAClick);
+        GameObject.Find("TargetsButton").GetComponent<Button>().onClick.AddListener(OnTargetsClick);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
 
     void FixedUpdate()
     {
         transform.Rotate(0, spinbot, 0, Space.World);
 
     }
+
+    void OnExitClick()
+    {
+        Application.Quit();
+    }
+
+    void OnCreditsClick()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+    void OnFFAClick()
+    {
+        SceneManager.LoadScene("Scene", LoadSceneMode.Single);
+        GameController.gamemode = 0;
+    }
+    void OnTargetsClick()
+    {
+        SceneManager.LoadScene("Scene", LoadSceneMode.Single);
+        GameController.gamemode = 1;
+    }
+
+
+
 }
